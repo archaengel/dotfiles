9b66ee94da2b8c895ae92f5f942cf444d73413d3
Archaengel <god11341258@gmail.com>
2022-03-20 21:00:03 -0700

    Add custom buffer picker config to telescope config file

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,2 +1,15 @@
-require('telescope').setup {}
+local actions = require('telescope.actions')
+
+require('telescope').setup {
+    pickers = {
+        buffers = {
+            attach_mappings = function(_, map)
+                map('n', 'dd', actions.delete_buffer)
+
+                return true
+            end
+        }
+    }
+}
 require('telescope').load_extension('fzy_native')
+
