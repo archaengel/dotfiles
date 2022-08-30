b39dd79c4e85afdbbaff166dc6039ecb3658a7f7
Edward Nuno <god11341258@gmail.com>
2022-08-30 13:39:11 -0700

    Clean up errant 'resolved' and rm unused code

--- nvim/.config/nvim/lua/archaengel/lsp/luaconfig.lua
+++ nvim/.config/nvim/lua/archaengel/lsp/luaconfig.lua
@@ -2,17 +2,6 @@ local lsputil = require 'archaengel.lsp.util'
 local custom_attach = lsputil.custom_attach
 local capabilities = lsputil.capabilities
 
-local system_name
-if vim.fn.has("mac") == 1 then
-    system_name = "macOS"
-elseif vim.fn.has("unix") == 1 then
-    system_name = "Linux"
-elseif vim.fn.has('win32') == 1 then
-    system_name = "Windows"
-else
-    print("Unsupported system for sumneko")
-end
-
 local libt = {}
 local function add(lib)
     for _, p in ipairs(vim.fn.expand(lib, false, true)) do
