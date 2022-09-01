473f825e784b2d4b222be244f665f7742352c4f5
Edward Nuno <god11341258@gmail.com>
2022-09-01 12:36:38 -0700

    Add dap and js adapter

---
+++ nvim/.config/nvim/after/plugin/dap.lua
@@ -0,0 +1,29 @@
+require("dap-vscode-js").setup({
+  adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+})
+
+for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
+  require("dap").configurations[language] = {
+      {
+        type = "pwa-node",
+        request = "launch",
+        name = "Launch file",
+        program = "${file}",
+        cwd = "${workspaceFolder}",
+      },
+      {
+        type = "pwa-node",
+        request = "attach",
+        name = "Attach",
+        processId = require'dap.utils'.pick_process,
+        cwd = "${workspaceFolder}",
+      },
+      {
+        type = "pwa-chrome",
+        request = "attach",
+        name = "Attach",
+        port = "9222",
+        cwd = "${workspaceFolder}",
+      },
+    }
+end
