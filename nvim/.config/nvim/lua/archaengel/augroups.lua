d43af24a473f1dd63c1ffd84143f411afca3c620
Edward Nuno <god11341258@gmail.com>
2022-09-09 09:08:56 -0700

    Fix formatting and add MD augroup

--- nvim/.config/nvim/lua/archaengel/augroups.lua
+++ nvim/.config/nvim/lua/archaengel/augroups.lua
@@ -26,15 +26,22 @@ local lsputil = require 'archaengel.lsp.util'
 local custom_attach = lsputil.custom_attach
 local capabilities = lsputil.capabilities
 
-local metals_config = require'metals'.bare_config()
+local metals_config = require 'metals'.bare_config()
 metals_config.capabilities = capabilities
 metals_config.on_attach = custom_attach
 
-local MetalsGroup = api.nvim_create_augroup("MetalsLsp", {clear = true})
+local MetalsGroup = api.nvim_create_augroup("MetalsLsp", { clear = true })
 api.nvim_create_autocmd("FileType", {
     pattern = "java,scala,sbt",
     callback = function()
         require('metals').initialize_or_attach(metals_config)
     end,
     group = MetalsGroup
+})
+
+local MarkdownGroup = api.nvim_create_augroup("SpellingMd", { clear = true })
+api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
+    command = "setlocal spell spelllang=en_us wrap",
+    group = MarkdownGroup
 })
