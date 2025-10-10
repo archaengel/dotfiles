894c0b33de228493abbe21e02e1e1c12feb2f06d
Eddie Nuno <GOD11341258@gmail.com>
2025-10-10 16:07:00 -0700

    Update nvim lspconfig (#56)

--- nvim/.config/nvim/lua/archaengel/lsp/kotlin.lua
+++ nvim/.config/nvim/lua/archaengel/lsp/kotlin.lua
@@ -4,15 +4,16 @@ local capabilities = lsputil.capablities
 local home = os.getenv('HOME')
 local kls_root_path = home .. '/third-party/kotlin-language-server'
 local kls_bin_path = kls_root_path ..
-                         '/server/build/install/server/bin/kotlin-language-server'
+    '/server/build/install/server/bin/kotlin-language-server'
 
-local settings = {kotlin = {compiler = {jvm = {target = "1.8"}}}}
+local settings = { kotlin = { compiler = { jvm = { target = "1.8" } } } }
 
-require('lspconfig').kotlin_language_server.setup {
+vim.lsp.config('kotlin_language_server', {
     -- Language server is currently broken when running with java 16
     capabilities = capabilities,
-    cmd_env = {JAVA_HOME = os.getenv('JAVA_15')},
-    cmd = {kls_bin_path},
+    cmd_env = { JAVA_HOME = os.getenv('JAVA_15') },
+    cmd = { kls_bin_path },
     on_attach = custom_attach,
     settings = settings
-}
+})
+vim.lsp.enable('kotlin_language_server')
