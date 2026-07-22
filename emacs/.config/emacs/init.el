7396196f76f1893b8a0f8578cb72e819fcdc6e01
edwardnuno <god11341258@gmail.com>
2026-07-22 11:10:51 -0700

    Add lsp, terminal, and grep config to emacs

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,14 +1,16 @@
 (require 'no-littering)
 (no-littering-theme-backups)
 
-(setq evil-want-C-u-scroll t)
+(setq
+ evil-want-C-u-scroll t
+ evil-vsplit-window-right 1)
 (require 'evil)
 (evil-mode 1)
 (evil-set-leader nil " ")
-(evil-define-key 'normal 'global (kbd "<leader>hf") 'describe-function)
-(evil-define-key 'normal 'global (kbd "<leader>hv") 'describe-variable)
+(evil-define-key '(normal visual motion) 'global (kbd "<leader>hf") 'describe-function)
+(evil-define-key '(normal visual motion) 'global (kbd "<leader>hv") 'describe-variable)
 (evil-define-key
-  'normal
+  '(normal visual motion)
   'global
   (kbd "<leader>en")
   ;; Hack to make configuring at runtime easier
@@ -20,6 +22,33 @@
 (evil-define-key '(normal visual motion) 'global (kbd "<leader>wv") 'evil-window-vsplit)
 (evil-define-key '(normal visual motion) 'global (kbd "<leader>ws") 'evil-window-split)
 (evil-define-key '(normal visual motion) 'global (kbd "<leader>SPC") 'find-file-at-point)
+(evil-define-key '(normal visual motion) 'global (kbd "<leader>lg") 'grep-find)
+(evil-define-key
+  '(normal visual motion)
+  'global
+  (kbd "<leader>rt")
+  (lambda()
+    (interactive)
+    (progn
+      (evil-window-vsplit)
+       (ghostel)
+      )))
+(add-hook
+ 'eglot-connect-hook
+ (progn
+   (evil-define-key '(normal visual motion) 'global (kbd "<leader>rf") 'xref-find-references)
+   (evil-define-key '(normal visual motion) 'global (kbd "<leader>rd") 'eglot-find-declaration)
+   (evil-define-key '(normal visual motion) 'global (kbd "<leader>ri") 'eglot-find-implementation)
+   (evil-define-key '(normal visual motion) 'global (kbd "<leader>rT") 'eglot-find-typeDeclaration)
+   ))
+
+
+(ido-mode 1)
+(with-eval-after-load 'grep
+    (grep-apply-setting
+    'grep-find-command
+    '("rg -n -H --no-heading -e '' $(git rev-parse --show-toplevel || pwd)" . 27)))
+
 
 (require 'nix-mode)
 (add-hook 'typescript-ts-mode-hook #'eglot-ensure)
